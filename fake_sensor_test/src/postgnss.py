efa84efe5b639d4d787118526a2a0eeb5fc6ace0
Timothy Parks <parkstimothyj@csu.fullerton.edu>
2019-04-07 21:21:18 -0700

    created fake sensor package and added rosbridge:

---
+++ fake_sensor_test/src/postgnss.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.7
+
+import rospy, time, signal
+from fake_sensor_test.msg import gps
+
+curr_pos = [33.881317,-117.882611]
+percision_lat = 0.000001
+percision_lon = -0.000001
+
+gps_pub = rospy.Publisher('/gnss', gps, queue_size=1)
+rospy.init_node('gnss')
+g = gps()
+
+g.baseLat = 33.881758
+g.baseLon = -117.882791
+
+def sigint_handler(signum, frame):
+    print 'CTRL+C Pressed!'
+    exit()
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+def fakeIt():
+    global curr_pos, percision_lat, percision_lon
+    while True:
+        #fake GPS Posting data
+        if curr_pos[0] > 33.881620 or curr_pos[0] < 33.881317:
+            percision_lat *= -1
+        if curr_pos[1] > -117.882611 or curr_pos[1] < -117.883346:
+            percision_lon *= -1 
+        curr_pos = (curr_pos[0] + percision_lat, curr_pos[1] + percision_lon)
+        g.roverLat, g.roverLon = curr_pos[0], curr_pos[1]
+        gps_pub.publish(g)
+        time.sleep(.2)
+
+if __name__ == '__main__':
+    fakeIt()
