8aa59dca626cbcf203f17233be7f3aef5b9deefe
Timothy Parks <parkstimothyj@csu.fullerton.edu>
2019-04-21 21:11:13 -0700

    add git-ignore

--- ce30_driver_ros-master/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/nvidia/catkin_ws/src/ce30_driver_ros-master/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
